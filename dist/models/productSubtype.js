52cfcd10d1ccc918b6ac1f1f493dec2a51ccda2c
anastasia-iteric <anastasia.kyrylenko@iteric.com>
2023-03-06 17:58:14 +0100

    Delete dist

--- dist/models/productSubtype.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const ProductSubtypeSchema = new mongoose_1.Schema({
-    subtype: { type: String },
-    title: { type: String },
-});
-exports.default = (0, mongoose_1.model)('ProductSubtype', ProductSubtypeSchema);
--- dist/models/productType.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const ProductTypeSchema = new mongoose_1.Schema({
-    type: { type: String },
-    title: { type: String },
-});
-exports.default = (0, mongoose_1.model)('ProductType', ProductTypeSchema);
